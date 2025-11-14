7b777e2876122d7b21e5de03fc41c933b9eb12f9
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-11-14 17:55:09 +0800

    project init

---
+++ src/test/java/com/loblivious/spring6restclient/Spring6RestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loblivious.spring6restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestClientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
