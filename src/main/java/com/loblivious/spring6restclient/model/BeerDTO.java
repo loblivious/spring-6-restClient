13d28de85b452a088a51988c8c5699f565a102f6
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-11-14 20:45:39 +0800

    created a page implementation for jackson to be able to de-serialize

---
+++ src/main/java/com/loblivious/spring6restclient/model/BeerDTO.java
@@ -0,0 +1,22 @@
+package com.loblivious.spring6restclient.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class BeerDTO {
+
+  private UUID id;
+  private Integer version;
+  private String beerName;
+  private BeerStyle beerStyle;
+  private String upc;
+  private Integer quantityOnHand;
+  private BigDecimal price;
+  private LocalDateTime createdDate;
+  private LocalDateTime updateDate;
+}
